8a49e17bdf3378095fe4f64c7a99d09b1c7e3fec
Tibin Sunny <33937416+Tibinsunny@users.noreply.github.com>
2019-10-06 23:39:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 
-Take over by Tibin Sunny
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>App Takeover by Tibin Sunny</title>
+
+	<!-- CSS -->
+	<!-- load bootstrap and our own personal stylesheet -->
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/superhero/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="container">
+	<div class="jumbotron">
+		<h1>This page is Owned by Tibin Sunny!</h1>
+		
+		<p>This is the sample of Page by <a href="http://www.TibinSunny.com">Tibin Sunny</a></p>
+		<a href="https://www.mTibinSunny.com" class="btn btn-primary btn-lg">View my blog </a>
+		<a href="https://hackerone.com/tibin_sunny" class="btn btn-primary btn-lg">View my HackerOne Profile </a>
+	
+
+
+	</div>
+</div>
+	
+</body>
+</html>
